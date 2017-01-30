785611ba8391d948081dd12e9c9569dadcf55732
Kevin Fodness <kevin@alleyinteractive.com>
2017-01-30 12:49:03 -0500

    AN-57 Added test for saving section mappings.

--- tests/admin/test-class-admin-apple-sections.php
+++ tests/admin/test-class-admin-apple-sections.php
@@ -23,7 +23,16 @@ class Admin_Apple_Sections_Test extends WP_UnitTestCase {
 	 */
 	public function setUp() {
 		parent::setup();
+
+		// Cache default settings for future use.
 		$this->settings = new Settings();
+
+		// Create some dummy categories to use in mapping testing.
+		wp_insert_term( 'Category 1', 'category' );
+		wp_insert_term( 'Category 2', 'category' );
+		wp_insert_term( 'Category 3', 'category' );
+
+		// Pre-cache a transient for sections using dummy data to bypass API call.
 		set_transient(
 			'apple_news_sections',
 			array(
@@ -78,8 +87,52 @@ class Admin_Apple_Sections_Test extends WP_UnitTestCase {
 		$this->assertEquals( 'post_tag', $taxonomy->name );
 	}
 
-	// TODO: Test filter for mapping taxonomy.
-	// TODO: Test save category mappings
+	/**
+	 * Ensures that the category mapping form saves properly.
+	 *
+	 * @access public
+	 */
+	public function testSaveCategoryMapping() {
+
+		// Get info about our categories.
+		$category1 = get_term_by( 'name', 'Category 1', 'category' );
+		$category2 = get_term_by( 'name', 'Category 2', 'category' );
+		$category3 = get_term_by( 'name', 'Category 3', 'category' );
+
+		// Set up post data.
+		$_POST = array(
+			'action' => 'apple_news_set_section_taxonomy_mappings',
+			'page' => 'apple_news_sections',
+			'taxonomy-mapping-abcdef01-2345-6789-abcd-ef012356789a' => array(
+				'Category 1',
+			),
+			'taxonomy-mapping-abcdef01-2345-6789-abcd-ef012356789b' => array(
+				'Category 2',
+			),
+		);
+		$_REQUEST = array(
+			'_wp_http_referer' => '/wp-admin/admin.php?page=apple-news-sections',
+			'_wpnonce' => wp_create_nonce( 'apple_news_sections' ),
+		);
+
+		// Run the request.
+		$sections = new Admin_Apple_Sections();
+		$sections->action_router();
+
+		// Validate the response.
+		$this->assertEquals(
+			array(
+				'abcdef01-2345-6789-abcd-ef012356789a' => array(
+					$category1->term_id
+				),
+				'abcdef01-2345-6789-abcd-ef012356789b' => array(
+					$category2->term_id
+				),
+			),
+			get_option( Admin_Apple_Sections::TAXONOMY_MAPPING_KEY )
+		);
+	}
+
 	// TODO: Test create post with categories in category mappings and ensure sections are properly set
 	// TODO: Test override functionality for manual settings selection
 }
