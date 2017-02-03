7b54ea737cca406133173144d019707569c92b59
Kevin Fodness <kevin@alleyinteractive.com>
2017-02-03 12:07:13 -0500

    AN-24 Refactor on Gallery_Test class for WordPress standards and PHP best practices.

--- tests/apple-exporter/components/test-class-gallery.php
+++ tests/apple-exporter/components/test-class-gallery.php
@@ -1,99 +1,57 @@
 <?php
+/**
+ * Publish to Apple News Tests: Gallery_Test class
+ *
+ * Contains a class which is used to test Apple_Exporter\Components\Gallery.
+ *
+ * @package Apple_News
+ * @subpackage Tests
+ */
 
 require_once __DIR__ . '/class-component-testcase.php';
 
-use Apple_Exporter\Components\Gallery as Gallery;
+use \Apple_Exporter\Components\Gallery;
 
+/**
+ * A class which is used to test the Apple_Exporter\Components\Gallery class.
+ */
 class Gallery_Test extends Component_TestCase {
 
-	public function testGeneratedJSON() {
-		$this->settings->set( 'use_remote_images', 'no' );
-
-		$workspace = $this->prophet->prophesize( '\Apple_Exporter\Workspace' );
-		// get_file_contents and write_tmp_files must be caleld with the specified params
-		$workspace->bundle_source( 'filename-1.jpg', 'http://someurl.com/filename-1.jpg' )->shouldBeCalled();
-		$workspace->bundle_source( 'another-filename-2.jpg', 'http://someurl.com/another-filename-2.jpg' )->shouldBeCalled();
-
-		// Pass the mock workspace as a dependency
-		$component = new Gallery( '<div class="gallery"><img
-			src="http://someurl.com/filename-1.jpg" alt="Example" /><img
-			src="http://someurl.com/another-filename-2.jpg" alt="Example" /></div>',
-			$workspace->reveal(), $this->settings, $this->styles, $this->layouts );
-
-		// Test for valid JSON
-		$this->assertEquals(
-			array(
-				'role' => 'gallery',
-				'items' => array(
-					array(
-						'URL' => 'bundle://filename-1.jpg',
-						'accessibilityCaption' => 'Example',
-					),
-					array(
-						'URL' => 'bundle://another-filename-2.jpg',
-						'accessibilityCaption' => 'Example',
-					),
-				),
-				'layout' => 'gallery-layout',
-			),
-			$component->to_array()
-		);
-	}
-
-	public function testGeneratedJSONRemoteImages() {
-		$this->settings->set( 'use_remote_images', 'yes' );
-
-		$workspace = $this->prophet->prophesize( '\Apple_Exporter\Workspace' );
-		// get_file_contents and write_tmp_files must be caleld with the specified params
-		$workspace->bundle_source( 'filename-1.jpg', 'http://someurl.com/filename-1.jpg' )->shouldNotBeCalled();
-		$workspace->bundle_source( 'another-filename-2.jpg', 'http://someurl.com/another-filename-2.jpg' )->shouldNotBeCalled();
-
-		// Pass the mock workspace as a dependency
-		$component = new Gallery( '<div class="gallery"><img
-			src="http://someurl.com/filename-1.jpg" alt="Example" /><img
-			src="http://someurl.com/another-filename-2.jpg" alt="Example" /></div>',
-			$workspace->reveal(), $this->settings, $this->styles, $this->layouts );
-
-		// Test for valid JSON
-		$this->assertEquals(
-			array(
-				'role' => 'gallery',
-				'items' => array(
-					array(
-						'URL' => 'http://someurl.com/filename-1.jpg',
-						'accessibilityCaption' => 'Example',
-					),
-					array(
-						'URL' => 'http://someurl.com/another-filename-2.jpg',
-						'accessibilityCaption' => 'Example',
-					),
-				),
-				'layout' => 'gallery-layout',
-			),
-			$component->to_array()
-		);
-	}
-
+	/**
+	 * Test the apple_news_gallery_json filter.
+	 *
+	 * @access public
+	 */
 	public function testFilter() {
+
+		// Setup.
 		$this->settings->set( 'use_remote_images', 'no' );
-
 		$workspace = $this->prophet->prophesize( '\Apple_Exporter\Workspace' );
-		// get_file_contents and write_tmp_files must be caleld with the specified params
-		$workspace->bundle_source( 'filename-1.jpg', 'http://someurl.com/filename-1.jpg' )->shouldBeCalled();
-		$workspace->bundle_source( 'another-filename-2.jpg', 'http://someurl.com/another-filename-2.jpg' )->shouldBeCalled();
-
-		// Pass the mock workspace as a dependency
-		$component = new Gallery( '<div class="gallery"><img
+		$workspace->bundle_source(
+			'filename-1.jpg',
+			'http://someurl.com/filename-1.jpg'
+		)->shouldBeCalled();
+		$workspace->bundle_source(
+			'another-filename-2.jpg',
+			'http://someurl.com/another-filename-2.jpg'
+		)->shouldBeCalled();
+		$component = new Gallery(
+			'<div class="gallery"><img
 			src="http://someurl.com/filename-1.jpg" alt="Example" /><img
 			src="http://someurl.com/another-filename-2.jpg" alt="Example" /></div>',
-			$workspace->reveal(), $this->settings, $this->styles, $this->layouts );
+			$workspace->reveal(),
+			$this->settings,
+			$this->styles,
+			$this->layouts
+		);
 
+		// Add the filter and set a custom layout.
 		add_filter( 'apple_news_gallery_json', function( $json ) {
 			$json['layout'] = 'fancy-layout';
 			return $json;
 		} );
 
-		// Test for valid JSON
+		// Ensure the filter properly modified the layout.
 		$this->assertEquals(
 			array(
 				'role' => 'gallery',
@@ -113,5 +71,99 @@ class Gallery_Test extends Component_TestCase {
 		);
 	}
 
-}
+	/**
+	 * Ensures that the component generates the proper JSON for a simple gallery.
+	 *
+	 * @access public
+	 */
+	public function testGeneratedJSON() {
 
+		// Setup.
+		$this->settings->set( 'use_remote_images', 'no' );
+		$workspace = $this->prophet->prophesize( '\Apple_Exporter\Workspace' );
+		$workspace->bundle_source(
+			'filename-1.jpg',
+			'http://someurl.com/filename-1.jpg'
+		)->shouldBeCalled();
+		$workspace->bundle_source(
+			'another-filename-2.jpg',
+			'http://someurl.com/another-filename-2.jpg'
+		)->shouldBeCalled();
+		$component = new Gallery(
+			'<div class="gallery"><img
+			src="http://someurl.com/filename-1.jpg" alt="Example" /><img
+			src="http://someurl.com/another-filename-2.jpg" alt="Example" /></div>',
+			$workspace->reveal(),
+			$this->settings,
+			$this->styles,
+			$this->layouts
+		);
+
+		// Test for valid JSON.
+		$this->assertEquals(
+			array(
+				'role' => 'gallery',
+				'items' => array(
+					array(
+						'URL' => 'bundle://filename-1.jpg',
+						'accessibilityCaption' => 'Example',
+					),
+					array(
+						'URL' => 'bundle://another-filename-2.jpg',
+						'accessibilityCaption' => 'Example',
+					),
+				),
+				'layout' => 'gallery-layout',
+			),
+			$component->to_array()
+		);
+	}
+
+	/**
+	 * Tests the functionality of the `use_remote_images` setting.
+	 *
+	 * @access public
+	 */
+	public function testGeneratedJSONRemoteImages() {
+
+		// Setup.
+		$this->settings->set( 'use_remote_images', 'yes' );
+		$workspace = $this->prophet->prophesize( '\Apple_Exporter\Workspace' );
+		$workspace->bundle_source(
+			'filename-1.jpg',
+			'http://someurl.com/filename-1.jpg'
+		)->shouldNotBeCalled();
+		$workspace->bundle_source(
+			'another-filename-2.jpg',
+			'http://someurl.com/another-filename-2.jpg'
+		)->shouldNotBeCalled();
+		$component = new Gallery(
+			'<div class="gallery"><img
+			src="http://someurl.com/filename-1.jpg" alt="Example" /><img
+			src="http://someurl.com/another-filename-2.jpg" alt="Example" /></div>',
+			$workspace->reveal(),
+			$this->settings,
+			$this->styles,
+			$this->layouts
+		);
+
+		// Ensure that the URL parameters are using remote images.
+		$this->assertEquals(
+			array(
+				'role' => 'gallery',
+				'items' => array(
+					array(
+						'URL' => 'http://someurl.com/filename-1.jpg',
+						'accessibilityCaption' => 'Example',
+					),
+					array(
+						'URL' => 'http://someurl.com/another-filename-2.jpg',
+						'accessibilityCaption' => 'Example',
+					),
+				),
+				'layout' => 'gallery-layout',
+			),
+			$component->to_array()
+		);
+	}
+}
