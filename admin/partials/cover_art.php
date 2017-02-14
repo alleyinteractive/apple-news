631d37fd565f4386ac51062d5487ac637592c03b
Kevin Fodness <kevin@alleyinteractive.com>
2017-02-14 15:13:23 -0500

    AN-45 Refinements to cover art language.

--- admin/partials/cover_art.php
+++ admin/partials/cover_art.php
@@ -8,7 +8,7 @@ $orientations = array(
 <p class="description">
 	<?php printf(
 		wp_kses(
-			__( 'You can set one or more <a href="%s">cover art</a> images below.', 'apple-news' ),
+			__( 'You can set one or more <a href="%s">cover art</a> images below. Only one image is required in order to enable cover art functionality.', 'apple-news' ),
 			array( 'a' => array( 'href' => array() ) )
 		),
 		'https://developer.apple.com/library/content/documentation/General/Conceptual/Apple_News_Format_Ref/CoverArt.html'
