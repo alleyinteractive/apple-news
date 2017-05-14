6617fb3cbb5f6f0b6d936a99c7708c532ae79359
Kevin Fodness <kevin@alleyinteractive.com>
2017-05-13 23:11:21 -0400

    AN-76 Cleanup on Admin_Apple_Themes_Test.

--- tests/admin/test-class-admin-apple-themes.php
+++ tests/admin/test-class-admin-apple-themes.php
@@ -1,30 +1,47 @@
 <?php
+/**
+ * Publish to Apple News Tests: Admin_Apple_Themes_Test class
+ *
+ * Contains a class which is used to test Admin_Apple_Themes.
+ *
+ * @package Apple_News
+ * @subpackage Tests
+ */
 
 use \Apple_Exporter\Settings as Settings;
 
+/**
+ * A class which is used to test the Admin_Apple_Themes class.
+ */
 class Admin_Apple_Themes_Test extends WP_UnitTestCase {
 
-	public function setup() {
-		parent::setup();
-
-		$themes = new \Admin_Apple_Themes();
-		delete_option( $themes->theme_key_from_name( 'Default' ) );
-		delete_option( $themes->theme_key_from_name( 'Test Theme' ) );
-
-		$this->settings = new Settings();
-	}
-
+	/**
+	 * A helper function to create the default theme.
+	 *
+	 * @access public
+	 */
 	public function createDefaultTheme() {
-		// Create default settings in the database
+
+		// Create default settings in the database.
 		$settings = new \Admin_Apple_Settings();
 		$settings->save_settings( $this->settings->all() );
 
-		// Force creation of a default theme
+		// Force creation of a default theme.
 		$themes = new \Admin_Apple_Themes();
 		$themes->setup_theme_pages();
 	}
 
+	/**
+	 * A helper function to create a new named theme.
+	 *
+	 * @param string $name The name for the theme.
+	 * @param array $settings The settings for the theme.
+	 *
+	 * @access public
+	 */
 	public function createNewTheme( $name, $settings = array() ) {
+
+		// Set up the request.
 		$nonce = wp_create_nonce( 'apple_news_save_edit_theme' );
 		$_POST['apple_news_theme_name'] = $name;
 		$_POST['action'] = 'apple_news_save_edit_theme';
@@ -33,28 +50,38 @@ class Admin_Apple_Themes_Test extends WP_UnitTestCase {
 		$_REQUEST['_wp_http_referer'] = '/wp-admin/admin.php?page=apple-news-theme-edit';
 		$_REQUEST['_wpnonce'] = $nonce;
 
-		// Merge any provided settings with default settings
+		// Merge any provided settings with default settings.
 		$current_settings = $this->settings->all();
 		$defaults = $this->getFormattingSettings( $current_settings );
 		$settings = wp_parse_args( $settings, $defaults );
 
-		// Add all of these to the $_POST object
+		// Add all of these to the $_POST object.
 		foreach ( $settings as $key => $value ) {
 			$_POST[ $key ] = $value;
 		}
 
+		// Invoke the save operation in the themes class.
 		$themes = new \Admin_Apple_Themes();
 		$themes->action_router();
 	}
 
+	/**
+	 * A helper function to extract formatting settings from general settings.
+	 *
+	 * @param array $all_settings An array of settings to filter.
+	 *
+	 * @access public
+	 * @return array The filtered set of formatting settings.
+	 */
 	public function getFormattingSettings( $all_settings ) {
-		// Get only formatting settings
+
+		// Get only formatting settings.
 		$formatting = new Admin_Apple_Settings_Section_Formatting( '' );
 		$formatting_settings = $formatting->get_settings();
-
 		$formatting_settings_keys = array_keys( $formatting_settings );
-		$filtered_settings = array();
 
+		// Loop through formatting settings and extract them from provided settings.
+		$filtered_settings = array();
 		foreach ( $formatting_settings_keys as $key ) {
 			if ( isset( $all_settings[ $key ] ) ) {
 				$filtered_settings[ $key ] = $all_settings[ $key ];
@@ -64,29 +91,41 @@ class Admin_Apple_Themes_Test extends WP_UnitTestCase {
 		return $filtered_settings;
 	}
 
-	public function testCreateTheme() {
-		// Set the POST data required to create a new theme
-		$name = 'Test Theme';
-		$this->createNewTheme( $name );
+	/**
+	 * Actions to be run before each test in this class.
+	 *
+	 * @access public
+	 */
+	public function setup() {
+		parent::setup();
 
-		// Check that the data was saved properly
+		// Remove the Default and Test Theme themes, if they exist.
 		$themes = new \Admin_Apple_Themes();
-		$current_settings = $this->settings->all();
+		delete_option( $themes->theme_key_from_name( 'Default' ) );
+		delete_option( $themes->theme_key_from_name( 'Test Theme' ) );
 
-		// Array diff against the option value
-		$diff_settings = $this->getFormattingSettings( $current_settings );
-		$new_theme_settings = get_option( $themes->theme_key_from_name( $name ) );
-		$this->assertEquals( $diff_settings, $new_theme_settings );
+		// Cache default settings for future use.
+		$this->settings = new Settings();
 	}
 
+	/**
+	 * Ensures that the default theme is created properly.
+	 *
+	 * @access public
+	 */
 	public function testCreateDefaultTheme() {
+
+		// Setup.
 		$themes = new \Admin_Apple_Themes();
 
-		// Create the default theme
+		// Create the default theme.
 		$this->createDefaultTheme();
 
-		// Ensure the default theme was created
-		$this->assertEquals( __( 'Default', 'apple-news' ), get_option( $themes::THEME_ACTIVE_KEY ) );
+		// Ensure the default theme was created.
+		$this->assertEquals(
+			__( 'Default', 'apple-news' ),
+			get_option( $themes::THEME_ACTIVE_KEY )
+		);
 		$this->assertEquals(
 			$this->getFormattingSettings( $this->settings->all() ),
 			get_option( $themes->theme_key_from_name( __( 'Default', 'apple-news' ) ) )
@@ -97,57 +136,53 @@ class Admin_Apple_Themes_Test extends WP_UnitTestCase {
 		);
 	}
 
-	public function testSetTheme() {
-		$themes = new \Admin_Apple_Themes();
+	/**
+	 * Ensures themes are able to be created properly.
+	 *
+	 * @access public
+	 */
+	public function testCreateTheme() {
 
-		// Create the default theme
-		$this->createDefaultTheme();
-
-		// Name a new theme
+		// Set the POST data required to create a new theme.
 		$name = 'Test Theme';
-
-		// Get Apple News settings and alter a setting to create a new theme
-		$settings_obj = new \Admin_Apple_Settings();
-		$settings = $settings_obj->fetch_settings()->all();
-		$settings['layout_margin'] = 50;
-		$settings_obj->save_settings( $settings );
 		$this->createNewTheme( $name );
 
-		// Simulate the form submission to set the theme
-		$nonce = wp_create_nonce( 'apple_news_themes' );
-		$_POST['action'] = 'apple_news_set_theme';
-		$_POST['apple_news_active_theme'] = $name;
-		$_POST['page'] = 'apple-news-themes';
-		$_REQUEST['_wp_http_referer'] = '/wp-admin/admin.php?page=apple-news-themes';
-		$_REQUEST['_wpnonce'] = $nonce;
-		$themes->action_router();
+		// Check that the data was saved properly.
+		$themes = new \Admin_Apple_Themes();
+		$current_settings = $this->settings->all();
 
-		// Check that the theme got set
-		$this->assertEquals( $name, get_option( $themes::THEME_ACTIVE_KEY ) );
-
-		$current_settings = $settings_obj->fetch_settings();
-		$this->assertEquals( 50, $current_settings['layout_margin'] );
+		// Array diff against the option value.
+		$diff_settings = $this->getFormattingSettings( $current_settings );
+		$new_theme_settings = get_option( $themes->theme_key_from_name( $name ) );
+		$this->assertEquals( $diff_settings, $new_theme_settings );
 	}
 
+	/**
+	 * Ensure that a theme can be deleted.
+	 */
 	public function testDeleteTheme() {
+
+		// Setup.
 		$themes = new \Admin_Apple_Themes();
 
-		// Create the default theme
+		// Create the default theme.
 		$this->createDefaultTheme();
 
-		// Name and create a new theme
+		// Name and create a new theme.
 		$name = 'Test Theme';
 		$this->createNewTheme( $name );
 
-		// Ensure both themes exist
+		// Ensure both themes exist.
 		$this->assertEquals(
 			array( __( 'Default', 'apple-news' ), $name ),
 			get_option( $themes::THEME_INDEX_KEY )
 		);
-		$this->assertNotEmpty( get_option( $themes->theme_key_from_name( __( 'Default', 'apple-news' ) ) ) );
+		$this->assertNotEmpty(
+			get_option( $themes->theme_key_from_name( __( 'Default', 'apple-news' ) ) )
+		);
 		$this->assertNotEmpty( get_option( $themes->theme_key_from_name( $name ) ) );
 
-		// Delete the test theme
+		// Delete the test theme.
 		$nonce = wp_create_nonce( 'apple_news_themes' );
 		$_POST['apple_news_theme_name'] = $name;
 		$_POST['action'] = 'apple_news_delete_theme';
@@ -157,7 +192,7 @@ class Admin_Apple_Themes_Test extends WP_UnitTestCase {
 		$_REQUEST['_wpnonce'] = $nonce;
 		$themes->action_router();
 
-		// Ensure both themes exist
+		// Ensure only the default theme exists after deletion.
 		$this->assertEquals(
 			array( __( 'Default', 'apple-news' ) ),
 			get_option( $themes::THEME_INDEX_KEY )
@@ -194,20 +229,23 @@ class Admin_Apple_Themes_Test extends WP_UnitTestCase {
 
 		// Add legacy format JSON overrides.
 		update_option(
-			'apple_news_json_body', array(
-				'apple_news_json_default-body' => $default_body,
-			),
+			'apple_news_json_body',
+			array( 'apple_news_json_default-body' => $default_body ),
 			false
 		);
 
 		// Run the function to trigger the settings migration.
-		$an = new Apple_News;
-		$an->upgrade_to_1_3_0();
+		$apple_news = new Apple_News;
+		$apple_news->migrate_custom_json_to_themes();
 
 		// Ensure that the default-body override was applied to the themes.
 		$themes = new \Admin_Apple_Themes();
-		$default_settings = get_option( $themes->theme_key_from_name( 'Default' ) );
-		$test_theme_settings = get_option( $themes->theme_key_from_name( 'Test Theme' ) );
+		$default_settings = get_option(
+			$themes->theme_key_from_name( __( 'Default', 'apple-news' ) )
+		);
+		$test_theme_settings = get_option(
+			$themes->theme_key_from_name( 'Test Theme' )
+		);
 		$this->assertEquals(
 			$default_settings['json_templates']['body']['default-body'],
 			$default_body
@@ -217,5 +255,42 @@ class Admin_Apple_Themes_Test extends WP_UnitTestCase {
 			$default_body
 		);
 	}
-}
 
+	/**
+	 * Ensure that a new theme can be set as the active theme.
+	 *
+	 * @access public
+	 */
+	public function testSetTheme() {
+
+		// Setup.
+		$themes = new \Admin_Apple_Themes();
+
+		// Create the default theme.
+		$this->createDefaultTheme();
+
+		// Name a new theme.
+		$name = 'Test Theme';
+
+		// Get Apple News settings and alter a setting to create a new theme.
+		$settings_obj = new \Admin_Apple_Settings();
+		$settings = $settings_obj->fetch_settings()->all();
+		$settings['layout_margin'] = 50;
+		$settings_obj->save_settings( $settings );
+		$this->createNewTheme( $name );
+
+		// Simulate the form submission to set the theme.
+		$nonce = wp_create_nonce( 'apple_news_themes' );
+		$_POST['action'] = 'apple_news_set_theme';
+		$_POST['apple_news_active_theme'] = $name;
+		$_POST['page'] = 'apple-news-themes';
+		$_REQUEST['_wp_http_referer'] = '/wp-admin/admin.php?page=apple-news-themes';
+		$_REQUEST['_wpnonce'] = $nonce;
+		$themes->action_router();
+
+		// Check that the theme got set.
+		$this->assertEquals( $name, get_option( $themes::THEME_ACTIVE_KEY ) );
+		$current_settings = $settings_obj->fetch_settings();
+		$this->assertEquals( 50, $current_settings['layout_margin'] );
+	}
+}
