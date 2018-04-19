1fc87ab919233d62db635e91f3c1635845a0936f
Morgan <manewc@gmail.com>
2018-04-19 15:27:51 -0400

    full class method, correct doc block

--- includes/apple-exporter/third-party/class-jetpack-tiled-gallery.php
+++ includes/apple-exporter/third-party/class-jetpack-tiled-gallery.php
@@ -24,7 +24,7 @@ class Jetpack_Tiled_Gallery {
 	 */
 	public static function instance() {
 		if ( ! isset( self::$instance ) ) {
-			self::$instance = new Jetpack_Tiled_Gallery;
+			self::$instance = new Jetpack_Tiled_Gallery();
 			self::$instance->setup();
 		}
 
@@ -33,8 +33,6 @@ class Jetpack_Tiled_Gallery {
 
 	/**
 	 * Setup of the singleton instance.
-	 *
-	 * @return void
 	 */
 	private function setup() {
 		// Only do this on export in Apple News context.
@@ -43,8 +41,6 @@ class Jetpack_Tiled_Gallery {
 
 	/**
 	 * Disable Jetpack's tiled gallery customizations.
-	 *
-	 * @return void
 	 */
 	public function tiled_gallery() {
 		// Condition for admin and Jetpack availability.
