6fc5a9eb49ebd4e907988815543be20e725658f6
Kevin Fodness <kevin@alley.co>
2018-05-08 16:36:42 -0400

    AN-87 Add basic support for tables

--- includes/apple-exporter/components/class-table.php
+++ includes/apple-exporter/components/class-table.php
@@ -57,182 +57,55 @@ class Table extends Component {
 		);
 	}
 
-	// TODO: REFACTOR LINE
-
 	/**
 	 * Build the component.
 	 *
-	 * @param string $text The text to convert into a component.
+	 * @param string $html The html to convert into a component.
 	 *
 	 * @access protected
 	 */
-	protected function build( $text ) {
+	protected function build( $html ) {
 
-		// Extract the URL from the text.
-		$url = self::url_from_src( $text );
-
-		/**
-		 * Allows for an image src value to be filtered before being applied.
-		 *
-		 * @param string $url The URL to be filtered.
-		 * @param string $text The raw text that was parsed for the URL.
-		 */
-		$url = esc_url_raw( apply_filters( 'apple_news_build_image_src', $url, $text ) );
-
-		// If we don't have a valid URL at this point, bail.
-		if ( empty( $url ) ) {
+		// If HTML is not enabled for this component, bail.
+		if ( ! $this->html_enabled() ) {
 			return;
 		}
 
-		// Add the URL as a parameter for replacement.
-		$filename = preg_replace( '/\\?.*/', '', \Apple_News::get_filename( $url ) );
-		$values = array(
-			'#url#'  => $this->maybe_bundle_source( $url, $filename ),
+		/**
+		 * Allows for table HTML to be filtered before being applied.
+		 *
+		 * @param string $html The raw HTML for the table.
+		 *
+		 * @since 1.4.0
+		 */
+		$html = apply_filters(
+			'apple_news_build_table_html',
+			$this->parser->parse( $html )
 		);
 
-		// Determine image alignment.
-		if ( false !== stripos( $text, 'align="left"' )
-		     || preg_match( '/class="[^"]*alignleft[^"]*"/i', $text )
-		) {
-			$this->set_anchor_position( Component::ANCHOR_LEFT );
-		} elseif ( false !== stripos( $text, 'align="right"' )
-		            || preg_match( '/class="[^"]*alignright[^"]*"/i', $text )
-		) {
-			$this->set_anchor_position( Component::ANCHOR_RIGHT );
-		} else {
-			$this->set_anchor_position( Component::ANCHOR_NONE );
+		// If we don't have any table HTML at this point, bail.
+		if ( empty( $html ) ) {
+			return;
 		}
 
-		// Check for caption
-		if ( preg_match( '#<figcaption.*?>(.*?)</figcaption>#m', $text, $matches ) ) {
-			$caption = trim( $matches[1] );
-			$values['#caption#'] = $caption;
-			$values = $this->group_component( $caption, $values );
-			$spec_name = 'json-with-caption';
-		} else {
-			$spec_name = 'json-without-caption';
-		}
-
-		// Full width images have top margin
-		// We can't use the standard layout registration due to grouping components
-		// with images so instead, send it through as a value.
-		if ( Component::ANCHOR_NONE === $this->get_anchor_position() ) {
-			$values = $this->register_non_anchor_layout( $values );
-		} else {
-			$values = $this->register_anchor_layout( $values );
-		}
-
-		// Register the JSON
-		$this->register_json( $spec_name, $values );
-	}
-
-	/**
-	 * Register the anchor layout.
-	 *
-	 * @param array $values
-	 * @return array
-	 * @access private
-	 */
-	private function register_anchor_layout( $values ) {
-		$this->register_layout(
-			'anchored-image',
-			'anchored-image'
-		);
-
-		$values['#layout#'] = 'anchored-image';
-		return $values;
-	}
-
-	/**
-	 * Register the non-anchor layout.
-	 *
-	 * @param array $values
-	 * @return array
-	 * @access private
-	 */
-	private function register_non_anchor_layout( $values ) {
-
-		// Get information about the currently loaded theme.
-		$theme = \Apple_Exporter\Theme::get_used();
-
-		// Set values to merge into the spec
-		$layout_values = array();
-
-		if ( 'yes' === $this->get_setting( 'full_bleed_images' ) ) {
-			$spec_name = 'non-anchored-full-bleed-image';
-		} else {
-			$layout_values['#layout_columns_minus_4#'] = $theme->get_layout_columns() - 4;
-			$spec_name = 'non-anchored-image';
-		}
-
-		// Register the layout.
-		$this->register_full_width_layout(
-			'full-width-image',
-			$spec_name,
-			$layout_values
-		);
-
-		$values['#layout#'] = 'full-width-image';
-		return $values;
-	}
-
-	/**
-	 * Find the caption alignment to use.
-	 *
-	 * @return string
-	 * @access private
-	 */
-	private function find_caption_alignment() {
-
-		// Get information about the currently loaded theme.
-		$theme = \Apple_Exporter\Theme::get_used();
-
-		$text_alignment = null;
-		if ( Component::ANCHOR_NONE === $this->get_anchor_position() ) {
-			return 'center';
-		}
-
-		switch ( $this->get_anchor_position() ) {
-			case Component::ANCHOR_LEFT:
-				return 'left';
-			case Component::ANCHOR_AUTO:
-				if ( 'left' === $theme->get_value( 'body_orientation' ) ) {
-					return 'right';
-				}
-		}
-
-		return 'left';
-	}
-
-	/**
-	 * If the image has a caption, we have to also show a caption component.
-	 * Let's instead, return the values as a Container instead of an Image.
-	 *
-	 * @param string $caption
-	 * @param array $values
-	 * @return array
-	 * @access private
-	 */
-	private function group_component( $caption, $values ) {
-
-		// Get information about the currently loaded theme.
-		$theme = \Apple_Exporter\Theme::get_used();
-
-		// Roll up the image component into a container.
-		$values = array_merge(
-			$values,
+		// Add the JSON for this component.
+		$this->register_json(
+			'json',
 			array(
-				'#caption#' => $caption,
-				'#text_alignment#' => $this->find_caption_alignment(),
-				'#caption_font#' => $theme->get_value( 'caption_font' ),
-				'#caption_size#' => intval( $theme->get_value( 'caption_size' ) ),
-				'#caption_tracking#' => intval( $theme->get_value( 'caption_tracking' ) ) / 100,
-				'#caption_line_height#' => intval( $theme->get_value( 'caption_line_height' ) ),
-				'#caption_color#' => $theme->get_value( 'caption_color' ),
-				'#full_bleed_images#' => ( 'yes' === $this->get_setting( 'full_bleed_images' ) ),
+				'#html#' => $html,
 			)
 		);
+	}
 
-		return $values;
+	/**
+	 * Whether HTML format is enabled for this component type.
+	 *
+	 * @param bool $enabled Optional. Whether to enable HTML support for this component. Defaults to true.
+	 *
+	 * @access protected
+	 * @return bool Whether HTML format is enabled for this component type.
+	 */
+	protected function html_enabled( $enabled = true ) {
+		return parent::html_enabled( $enabled );
 	}
 }
