11a9e16cdbd8287f2d447e36e16aa1ff7e28856d
Paul Schreiber <paul.schreiber@espn.com>
2018-08-03 16:05:11 -0400

    add 'input var ok' comments

--- admin/class-admin-apple-themes.php
+++ admin/class-admin-apple-themes.php
@@ -269,8 +269,8 @@ class Admin_Apple_Themes extends Apple_News {
 		// Negotiate theme object.
 		$error = '';
 		$theme = new \Apple_Exporter\Theme();
-		if ( isset( $_GET['theme'] ) ) {
-			$theme_name = sanitize_text_field( wp_unslash( $_GET['theme'] ) );
+		if ( isset( $_GET['theme'] ) ) { // input var ok.
+			$theme_name = sanitize_text_field( wp_unslash( $_GET['theme'] ) ); // input var ok.
 			$theme->set_name( $theme_name );
 			if ( false === $theme->load() ) {
 				$error = sprintf(
