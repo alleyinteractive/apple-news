28371e219ee5065640fea27a8141b5ddb6ef10fd
kevinfodness <kevin@kevinfodness.com>
2019-07-19 13:53:31 -0400

    AN-166 Augment list of settings returned by the API

--- assets/js/pluginsidebar/components/sidebar/index.js
+++ assets/js/pluginsidebar/components/sidebar/index.js
@@ -523,23 +523,27 @@ class Sidebar extends React.PureComponent {
             )
           }
         </PanelBody>
-        <PanelBody
-          initialOpen={false}
-          title={__('Apple News Publish Information', 'apple-news')}
-        >
-          <h4>{__('API Id', 'apple-news')}</h4>
-          <p>{apiId}</p>
-          <h4>{__('Created On', 'apple-news')}</h4>
-          <p>{dateCreated}</p>
-          <h4>{__('Last Updated On', 'apple-news')}</h4>
-          <p>{dateModified}</p>
-          <h4>{__('Share URL', 'apple-news')}</h4>
-          <p>{shareUrl}</p>
-          <h4>{__('Revision', 'apple-news')}</h4>
-          <p>{revision}</p>
-          <h4>{__('Publish State', 'apple-news')}</h4>
-          <p>{publishState}</p>
-        </PanelBody>
+        {'' !== publishState && 'N/A' !== publishState ? (
+          <PanelBody
+            initialOpen={false}
+            title={__('Apple News Publish Information', 'apple-news')}
+          >
+            <h4>{__('API Id', 'apple-news')}</h4>
+            <p>{apiId}</p>
+            <h4>{__('Created On', 'apple-news')}</h4>
+            <p>{dateCreated}</p>
+            <h4>{__('Last Updated On', 'apple-news')}</h4>
+            <p>{dateModified}</p>
+            <h4>{__('Share URL', 'apple-news')}</h4>
+            <p>{shareUrl}</p>
+            <h4>{__('Revision', 'apple-news')}</h4>
+            <p>{revision}</p>
+            <h4>{__('Publish State', 'apple-news')}</h4>
+            <p>{publishState}</p>
+          </PanelBody>
+        ) : (
+          <div>Publish Me!</div>
+        )}
       </PluginSidebar>
     );
   }
