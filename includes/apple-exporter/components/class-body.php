42f3ba3e1f448ccb1fa0b7ec12a6271ee4204e82
David Herrera <david@alley.com>
2024-05-05 22:18:51 -0400

    Fix malformed tokens

--- includes/apple-exporter/components/class-body.php
+++ includes/apple-exporter/components/class-body.php
@@ -477,7 +477,7 @@ class Body extends Component {
 					'fontSize'   => '#cite_size#',
 					'tracking'   => '#cite_tracking#',
 					'lineHeight' => '#cite_line_height#',
-					'textColor'  => '#cite_color',
+					'textColor'  => '#cite_color#',
 				],
 				$conditionals['cite'] ?? []
 			),
@@ -487,7 +487,7 @@ class Body extends Component {
 					'fontSize'   => '#monospaced_size#',
 					'tracking'   => '#monospaced_tracking#',
 					'lineHeight' => '#monospaced_line_height#',
-					'textColor'  => '#monospaced_color',
+					'textColor'  => '#monospaced_color#',
 				],
 				$conditionals['monospaced'] ?? []
 			),
