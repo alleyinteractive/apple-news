097cf63748f659ddd9b76b099ee48e375270be95
efuller <eric@alley.co>
2024-11-14 16:23:37 -0500

    Add section mappings entry point as a subfolder of admin settings

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = (env, { mode }) => ({
   entry: {
     pluginSidebar: './assets/js/pluginsidebar/index.jsx',
     adminSettings: './assets/js/admin-settings/frontend.jsx',
-    sectionMappings: './assets/js/admin-section-mappings/index.jsx',
+    sectionMappings: './assets/js/admin-settings/section-mappings/index.jsx',
   },
 
   // Configure loaders based on extension.
