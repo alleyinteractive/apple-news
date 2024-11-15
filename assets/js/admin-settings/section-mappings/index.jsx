dacf48f10dd9ca25273fe65a6bba8eb3edc0670b
efuller <eric@alley.co>
2024-11-15 09:18:22 -0500

    Add in initial mappings layout

--- assets/js/admin-settings/section-mappings/index.jsx
+++ assets/js/admin-settings/section-mappings/index.jsx
@@ -1,11 +1,12 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
+import Mappings from './mappings';
 
 const container = document.getElementById('apple-news-options__section-mappings');
 const root = ReactDOM.createRoot(container);
 
 root.render(
   <StrictMode>
-    <h1>Section Mappings</h1>
+    <Mappings />
   </StrictMode>,
 );
