b6cbad50dad31a7b72dcf3ca34758a73ad715ea4
efuller <eric@alley.co>
2024-11-18 09:05:09 -0500

    Add in ability to add new section rule

--- assets/js/admin-settings/index.jsx
+++ assets/js/admin-settings/index.jsx
@@ -40,11 +40,11 @@ function AdminSettings() {
   /**
    * Adds a new empty rule to the end of the list.
    */
-  const addRule = () => {
+  const addRule = (field = '') => {
     updateSettings([
       ...(ruleList ?? []),
       {
-        field: '',
+        field,
         taxonomy: '',
         term_id: 0,
         value: '',
@@ -118,7 +118,9 @@ function AdminSettings() {
     <div className="apple-news-options__wrapper">
       <h1>{__('Apple News Automation', 'apple-news')}</h1>
       <p>{__('Configure automation rules below to automatically apply certain settings based on the taxonomy terms applied to each post.', 'apple-news')}</p>
-      <p><a target="_blank" rel="noreferrer" href="https://github.com/alleyinteractive/apple-news/wiki/Automation">{__('For more information on how automation works, visit our wiki.', 'apple-news')}</a></p>
+      <p>
+        <a target="_blank" rel="noreferrer" href="https://github.com/alleyinteractive/apple-news/wiki/Automation">{__('For more information on how automation works, visit our wiki.', 'apple-news')}</a>
+      </p>
       <table className="wp-list-table widefat fixed striped">
         <thead>
           <tr>
@@ -130,32 +132,42 @@ function AdminSettings() {
           </tr>
         </thead>
         <tbody>
-          {!loading && ruleList ? (
-            ruleList.map((item, index) => (
-              <Rule
-                busy={busy}
-                field={item.field}
-                key={index} // eslint-disable-line react/no-array-index-key
-                onDelete={() => updateSettings(deleteAtIndex(ruleList, index))}
-                onDragEnd={(e) => {
-                  const targetRow = document
-                    .elementFromPoint(e.clientX, e.clientY)
-                    .closest('.apple-news-automation-row');
-                  if (targetRow) {
-                    reorderRule(
-                      index,
-                      Array.from(targetRow.parentElement.querySelectorAll('tr'))
-                        .indexOf(targetRow),
-                    );
-                  }
-                }}
-                onUpdate={(key, value) => updateRule(index, key, value)}
-                taxonomy={item.taxonomy}
-                termId={item.term_id}
-                value={item.value}
-              />
-            ))
-          ) : null}
+          {sectionRows}
+        </tbody>
+      </table>
+      <div className="tablenav bottom">
+        <div className="alignleft actions">
+          <Button
+            disabled={busy}
+            isSecondary
+            onClick={() => addRule('links.sections')}
+            style={{ marginTop: '10px' }}
+          >
+            {__('Add Rule', 'apple-news')}
+          </Button>
+          {' '}
+          <Button
+            disabled={busy}
+            isPrimary
+            onClick={saveSettings}
+          >
+            {__('Save Settings', 'apple-news')}
+          </Button>
+        </div>
+      </div>
+      <h2 className="title">Additional Automation</h2>
+      <table className="wp-list-table widefat fixed striped">
+        <thead>
+          <tr>
+            <th id="apple-news-automation-column-taxonomy" scope="col">{__('Taxonomy', 'apple-news')}</th>
+            <th id="apple-news-automation-column-term" scope="col">{__('Term', 'apple-news')}</th>
+            <th id="apple-news-automation-column-field" scope="col">{__('Field', 'apple-news')}</th>
+            <th id="apple-news-automation-column-value" scope="col">{__('Value', 'apple-news')}</th>
+            <th id="apple-news-automation-column-delete" scope="col">{__('Delete?', 'apple-news')}</th>
+          </tr>
+        </thead>
+        <tbody>
+          {nonSectionRows}
         </tbody>
       </table>
       <div className="tablenav bottom">
