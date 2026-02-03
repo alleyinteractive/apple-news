fad76a077fdc94162265a76e730dbe42c31444bc
renatonascalves <renatonascalves@users.noreply.github.com>
2026-02-03 17:18:53 +0000

    Built changes from nyp 3c62836abe04dbca74cc80438a592ac7cc53a115

---
+++ build/pluginSidebar.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-i18n', 'wp-plugins', 'wp-url'), 'version' => '46d8f300c61cc0fa7a9c');
